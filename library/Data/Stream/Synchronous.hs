41a179249590e558343ec62e19f384ab98b781a5
yumiova <yumiova@protonmail.ch>
2019-11-10 22:10:49 +0100

    Implement monadic instances for stream-sources

--- library/Data/Stream/Synchronous.hs
+++ library/Data/Stream/Synchronous.hs
@@ -1,7 +1,15 @@
-module Data.Stream.Synchronous (Stream, Source) where
+{-# LANGUAGE TupleSections #-}
 
+module Data.Stream.Synchronous
+  ( Stream,
+    Source,
+  )
+where
+
+import Control.Applicative (liftA2)
 import Control.Monad.Fix (MonadFix (mfix))
 import Control.Monad.ST (ST)
+import Data.Bifunctor (first, second)
 
 newtype Stream t a = Stream {runStream :: ST t a}
 
@@ -24,3 +32,20 @@ instance MonadFix (Stream t) where
   mfix f = Stream $ mfix $ runStream . f
 
 newtype Source t a = Source {runSource :: ST t (a, ST t (ST t ()))}
+
+instance Functor (Source t) where
+  fmap f = Source . fmap (first f) . runSource
+
+instance Applicative (Source t) where
+
+  pure = Source . pure . (,mempty)
+
+  (<*>) fsource = Source . liftA2 merge (runSource fsource) . runSource
+    where
+      merge ~(f, fgather) ~(a, gather) = (f a, fgather <> gather)
+
+instance Monad (Source t) where
+  lsource >>= f =
+    Source $ do
+      ~(a, lgather) <- runSource lsource
+      second (lgather <>) <$> runSource (f a)
