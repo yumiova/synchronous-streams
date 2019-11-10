13d935bdd9019a1b3af155918bbe285882bfc11f
yumiova <yumiova@protonmail.ch>
2019-11-10 21:11:13 +0100

    Import pinned version of Nixpkgs

--- release.nix
+++ release.nix
@@ -1,1 +1,14 @@
+let
+  channel = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
+
+  gitRevision = rev: "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+
+  nixpkgs = builtins.fetchTarball {
+    name = "nixpkgs";
+    url = gitRevision channel.rev;
+    inherit (channel) sha256;
+  };
+
+  pkgs = import nixpkgs {};
+in
 {}
