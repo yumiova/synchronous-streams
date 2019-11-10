058d243d7981365bea3ae6776bb98549b8e10177
yumiova <yumiova@protonmail.ch>
2019-11-10 21:35:58 +0100

    Implement monadic instances for stream-views

--- library/Data/Stream/Synchronous.hs
+++ library/Data/Stream/Synchronous.hs
@@ -3,3 +3,18 @@ module Data.Stream.Synchronous (Stream) where
 import Control.Monad.ST (ST)
 
 newtype Stream t a = Stream {runStream :: ST t a}
+
+instance Functor (Stream t) where
+  fmap f = Stream . fmap f . runStream
+
+instance Applicative (Stream t) where
+
+  pure = Stream . pure
+
+  (<*>) fstream = Stream . (<*>) (runStream fstream) . runStream
+
+instance Monad (Stream t) where
+  lstream >>= f =
+    Stream $ do
+      a <- runStream lstream
+      runStream (f a)
