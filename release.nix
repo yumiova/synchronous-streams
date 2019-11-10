04acb45ea8037d9f449481934c5ac1b490b0fd8e
yumiova <yumiova@protonmail.ch>
2019-11-10 21:12:20 +0100

    Overlay package derivation into Nixpkgs channel

--- release.nix
+++ release.nix
@@ -1,4 +1,14 @@
 let
+  overlay = self: super: {
+    haskellPackages =
+      super.haskellPackages.extend (
+        haskellPackagesSelf: haskellPackagesSuper: {
+          synchronous-streams =
+            haskellPackagesSelf.callCabal2nix "synchronous-streams" ./. {};
+        }
+      );
+  };
+
   channel = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
 
   gitRevision = rev: "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
@@ -9,6 +19,6 @@ let
     inherit (channel) sha256;
   };
 
-  pkgs = import nixpkgs {};
+  pkgs = import nixpkgs { overlays = [ overlay ]; };
 in
 {}
