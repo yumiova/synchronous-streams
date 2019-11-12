4c086386a888c32040c558d414c5043d2017c363
yumiova <yumiova@protonmail.ch>
2019-11-12 21:41:00 +0100

    Define primitive for interleaving effects over functors

--- library/Control/Monad/Primitive/Unsafe.hs
+++ library/Control/Monad/Primitive/Unsafe.hs
@@ -1,1 +1,17 @@
-module Control.Monad.Primitive.Unsafe () where
+{-# LANGUAGE UnboxedTuples #-}
+
+module Control.Monad.Primitive.Unsafe
+  ( unsafeDupableCollect,
+  )
+where
+
+import Control.Monad.Primitive (PrimBase (internal), PrimMonad (primitive))
+
+unsafeDupableCollect :: (Functor f, PrimBase m) => (a -> m b) -> f a -> m (f b)
+unsafeDupableCollect f action =
+  primitive $ \world ->
+    (#
+      world,
+      (\a -> let (# _, b #) = internal (f a) world in b) <$> action
+    #)
+{-# NOINLINE unsafeDupableCollect #-}
